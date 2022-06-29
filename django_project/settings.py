a84fecbf2c1fcdae89331d26a135abf3c47d532c
ZolsKe <zoltan.the.kepes@gmail.com>
2022-06-29 23:05:14 +0100

    set DEBUG = True

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates') #make sure 'os' is imported
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['salt-and-pepper-manager.herokuapp.com', 'localhost', '127.0.0.1']
 
